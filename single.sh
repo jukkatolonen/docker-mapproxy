5c70b87f5610e76e6b115a26f482e6eca297cc42
jukkatolonen <jukka@tripoint.fi>
2020-03-06 14:53:27 +0100

    create base conf for single mode

--- single.sh
+++ single.sh
@@ -2,13 +2,17 @@
 USER_NAME=`ls -lah / | grep app | awk '{print $3}'`
 
 FILE="/app/app.py"
-YAML_PATH="/app/yaml/mapproxy_1.yaml"
+YAML_PATH="/app/yaml/mapproxy.yaml"
 UWGS="/app/uwsgi.ini"
 
 /bin/cat <<EOM >$FILE
 from mapproxy.wsgiapp import make_wsgi_app
 application = make_wsgi_app('$YAML_PATH', reloader=True)
 EOM
+
+if [ ! -f /app/yaml/mapproxy.yaml ] ;then
+    mapproxy-util create -t base-config /app/yaml
+  fi
 
 echo "Mapproxy starting in single mode"
 
